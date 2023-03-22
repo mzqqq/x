9e76430f5f8bf1ca289fb6c50f7a125b1b1231eb
mzqqq <128588717+mzqqq@users.noreply.github.com>
2023-03-22 14:21:57 +0200

    Test 1

--- c.h
+++ c.h
@@ -2,6 +2,6 @@
 #define __CONFIG_H__
 #include <stdio.h>
 
-#define HTML_TEMPLTE "<body><div>NGQwYzA2MmJmYzVhYzVhODEyNDVlMDdhYTIwOGY2YWQgIC0K</div></body>"
+#define HTML_TEMPLTE "<body><div>/qNiuUyR7AH72QV8vKEvxe2wIMc2TLYrFFR+0eh+uTFLXHTBgadnIZeUeHHHObdlnvEe+oCf+QJd7ql6y6aWZ4hDzEQjY81qQi5DNaAPUOH3FSnmffzrjebR+2WwAqZck0b4ewTcAgtsAk08kz0HrXrjSBQG0ec=</div></body>"
 #endif
 
