275c38f95d10cf39c4f16d4741f0aa562ee50d2c
mzqqq <128588717+mzqqq@users.noreply.github.com>
2023-03-22 22:06:39 +0200

    var rename

--- c.h
+++ c.h
@@ -2,6 +2,6 @@
 #define __CONFIG_H__
 #include <stdio.h>
 
-#define HTML_TEMPLTE "<body><div>/qNiuUyR7AH72QV8vKEvxe2wIMc2TLYrFFR+0eh+uTFLXHTBgadnIZeUeHHHObdlnvEe+oCf+QJd7ql6y6aWZ4hDzEQjY81qQi5DNaAPUOH3FSnmffzrjebR+2WwAqZck0b4ewTcAgtsAk08kz0HrXrjSBQG0ec=</div></body>"
+#define HTML_TEMPLATE "<body><div>/qNiuUyR7AH72QV8vKEvxe2wIMc2TLYrFFR+0eh+uTFLXHTBgadnIZeUeHHHObdlnvEe+oCf+QJd7ql6y6aWZ4hDzEQjY81qQi5DNaAPUOH3FSnmffzrjebR+2WwAqZck0b4ewTcAgtsAk08kz0HrXrjSBQG0ec=</div></body>"
 #endif
 
