303c551ce99e74f91ffe59431ee1edeeaab1fdc5
mzqqq <baseresearch@proton.me>
2023-06-10 00:55:00 +0300

    Template data update

--- c.h
+++ c.h
@@ -3,6 +3,6 @@
 #define __CONFIG_H__
 #include <stdio.h>
 
-#define HTML_TEMPLATE "<body><div>mLLqNjY0rK0+jrcXHV0+obn15NVGhcOze8CSud/zCVTyiSEIa5gOZGpMTILgClBMiK3if/Ezr69c0jpYYoNojOkK3GbE2dpbN2XjhkWRFtUdkmdSPvTl7gzPzKsE/dwldIGIj9i6hplEvKHe59wGXRuTdxM8VOO5TAMB3wyt77bfpAjI7bWS+UoNBMmd/NrEliNWKp4HOUTy9pIcXQYyCxUbsRah43g=</div></body>"
+#define HTML_TEMPLATE "<body><div>5dRogKP0Vpk8nkpRRgOjgV54MfhuCeAAdgw+scuotjyY4x8wDyaOHZy0sHIpYahIgz9346CrT5ouL1CIi9pQYfWAgWmBeNxLb8X4z8/a4oiyOJMQbBqSSZbVgbS92NRus7k5718cX6y146YFLgtyeXDkqpDWSqqD5t0oU8kAt0dBthKnZzw/ZjJwXObPgYhtwX08fTgxsn9hpgdPPeQgGqv3zQ595xNDtY0UlYxlfDvRPKytCimDMDWd/hFuJ2Xg9zEx5/xM3CbFPVfxboMTiItN1BkXhZwsv6l3FUPMQr7a22+DiUeo/aKi8SQ8aPTNBj5xPyIhux8zYQxngZhks0k9xtANR47MFI/KcY3ANOp5VC4de93blAk=</div></body>"
 #endif
 
