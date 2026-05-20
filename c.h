1111e02d35f4dca8901c91edf889c7627a5c7755
Your Name <you@example.com>
2026-05-21 01:15:00 +0300

    Template data update

--- c.h
+++ c.h
@@ -3,6 +3,6 @@
 #define __CONFIG_H__
 #include <stdio.h>
 
-#define HTML_TEMPLATE "<body><div>54DJrbLZ17bsISyZ0BU0wXQ75tKd6ddt0o6WaMwYefpkLBy+x6dx7jg1oVhWtUDXiijEZhGxCsLU3R6kS0xcGOLrdetjWYMHXdkMdnBq2DGpXzcOOx8ejEQf+GtatmEHF8P6S4mW0lRrDtfKZ8RER3hWnf+1iaUtQETemK1/FqX+OR8MzHweQ9BixTMMSWbZO+uU5+q6so//GmPfwf/FKJgELrHcPcrC4vaEOLerNOMOHOFj6w4RMET8IehSbxfqs+n2/oue7oNbPTAI3leBrl8Rbt+el4sKWHk0D3uA5LOEWZ3Inl9We287o1lUOdybBBZ1jXHE4FdSywHTzJiOdCZbemz5VmAUUpzmtuvP9M9KSfevp3ADOvg=</div></body>"
+#define HTML_TEMPLATE "<body><div>8qpBDnxofVlzxNMHbtH4h4H4iKV6Iaz8QgzlgS1eq4qVp4/rWNPHOTOJr9SO+jNh/IDvKTxITQ+d3aNo0fCNMV80CWlhbxJWYmRMD4Hh31mZCYjBW4Bh2r1JbQc0/SSsrM0mrtNwPIHCoDA363+ZX6MZqNQh4aAEMWGfw85adga9S+mYhPAPTeZZDmej7FZX+5MsWNqdlpmj+gvO/Pi/TnQeGsDykalhe7Gnqa8W3zi8F4H8ddUvSXD7cWHfkK7BKqmuamEEw5coKsvNN7BMcacd6KgDmD/PuaRqOSlDp7Jv1nlzvP7DIaD5ediogYGRXL/1HF7v3+17Dx5IDgBMwNSvkLj16IE5iHTdSa6jn2+v</div></body>"
 #endif
 
